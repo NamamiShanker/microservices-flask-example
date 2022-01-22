e623c2a4d59c62e0aa2dc8aa98a5d9829b53cc04
Namami Shanker <namami2011@gmail.com>
2022-01-22 12:25:21 +0530

    Update content start script

--- content/start-Docker.sh
+++ content/start-Docker.sh
@@ -6,6 +6,6 @@ poetry shell
 export FLASK_APP=manage.py
 export $(grep -v '^#' .flaskenv-Docker | xargs)
 sleep 5s
-flask db upgrade
+python3 -m flask db upgrade
 python3 -m celery --app manage.celery worker --loglevel INFO --detach
 gunicorn -b 0.0.0.0:5000 manage:app --workers 1 --timeout 60
