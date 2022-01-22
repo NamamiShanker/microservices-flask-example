a69712e8da7aaba405783f053370f4abc5f01ebe
Namami Shanker <namami2011@gmail.com>
2022-01-22 18:06:21 +0530

    Remove waiting time from start script

--- interaction/start-Docker.sh
+++ interaction/start-Docker.sh
@@ -5,7 +5,6 @@ set -o nounset
 poetry shell
 export FLASK_APP=manage.py
 export $(grep -v '^#' .flaskenv-Docker | xargs)
-sleep 5s
 python3 -m flask db upgrade
 gunicorn -b 0.0.0.0:5000 manage:app --workers 1 --timeout 60
 
